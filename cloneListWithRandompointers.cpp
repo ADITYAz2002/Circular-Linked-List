b353e8e45644b717ef408eb7b9c7ad92e7bdd349
Aditya Chaturvedi <adityachaturvedi02032003@gmail.com>
2023-08-26 13:52:35 +0530

    Clone a Linked List with Random Pointer.

---
+++ cloneListWithRandompointers.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+struct Node 
+{ 
+    int data; 
+    Node *next,*random; 
+    Node(int x) 
+    { 
+        data = x; 
+        next = random = NULL; 
+    } 
+}; 
+
+void print(Node *start) 
+{ 
+    Node *ptr = start; 
+    while (ptr) 
+    { 
+        cout << "Data = " << ptr->data << ", Random  = "<< ptr->random->data << endl; 
+        ptr = ptr->next; 
+    } 
+}
+
+Node* clone(Node* head)
+{
+    unordered_map<Node*, Node*> mp;
+
+    for(Node* curr = head; curr != NULL; curr = curr -> next)
+        mp[curr] = new Node(curr -> data);
+    
+    for(Node* curr = head; curr != NULL; curr = curr -> next)
+    {
+        mp[curr] -> next = mp[curr -> next];
+        mp[curr] -> random =  mp[curr -> random];
+    }
+
+    Node* head2 = mp[head];
+
+    return head;
+}
+
+int main() 
+{ 
+	Node* head = new Node(10); 
+    head->next = new Node(5); 
+    head->next->next = new Node(20); 
+    head->next->next->next = new Node(15); 
+    head->next->next->next->next = new Node(20); 
+    
+    head->random = head->next->next;
+    head->next->random=head->next->next->next;
+    head->next->next->random=head;
+    head->next->next->next->random=head->next->next;
+    head->next->next->next->next->random=head->next->next->next;
+
+    cout << "Original list : \n"; 
+    print(head); 
+    
+    cout << "\nCloned list : \n"; 
+    Node *cloned_list = clone(head); 
+    print(cloned_list); 
+  
+    return 0; 
+} 
